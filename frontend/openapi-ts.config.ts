fc2782ba54d30ff9c57172e651156eb0a84582ef
Manoj Sethi <manojsethi90@gmail.com>
2025-08-28 00:29:51 +0530

    Unused file removed

--- frontend/openapi-ts.config.ts
+++
@@ -1,7 +0,0 @@
-// openapi.config.ts
-import { defineConfig } from "@hey-api/openapi-ts";
-
-export default defineConfig({
-  input: "http://localhost:8000/api/v1/openapi.json", // URL to your backend OpenAPI JSON
-  output: "src/api/generated",                        // folder where types will go
-});
